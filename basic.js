a9f181f1fe528e74633a61c462c635ec8ab3b588
kiransamjhana <kiranmanandhar956@gmail.com>
2023-06-04 13:16:00 +1000

    basic backedn

--- basic.js
+++ basic.js
@@ -1,5 +1,31 @@
-import express from "express"
-const app= express();
+// import express from "express";
+// const app = express();
 
+// // const EventEmitter = require("events");
+// import { EventEmitter } from "events";
+// console.log("first");
 
-const EventEmitter= require("events")
+// const eemter = new EventEmitter();
+
+// eemter.on("premGo", () => {
+//   console.log("this is inside eemetr.on. so you have fired premG event");
+
+//   eemter.emit("premCome");
+// });
+
+// eemter.on("premCome", () => {
+//   console.log("coming home");
+// });
+
+// app.get("/register", (req, res) => {});
+
+// app.get("/", (req, res) => {
+//   res.send("<h1>hehehe</h1>");
+//   eemter.emit("premGo");
+// });
+
+// app.listen(8000, (error) => {
+//   error
+//     ? console.log(error.message)
+//     : console.log("your server is running at http://localhost:8000");
+// });
