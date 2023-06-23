966f9050e1218de416e580e2f78c358ffb1b3ead
kiransamjhana <kiranmanandhar956@gmail.com>
2023-06-24 09:58:55 +1000

    new file

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ import express from "express";
 import cors from "cors";
 const app = express();
 
-app.use(cors());
 const PORT = 8080;
+import path from "path";
 
+const _dirname = path.resolve();
+console.log(_dirname);
 import { mongoConnect } from "./src/config/mongoDb.js";
 mongoConnect();
 
 // middleware
 app.use(express.json());
+app.use(cors());
+app.use(express.static(_dirname + "/build"));
 
 //api endpoints
 import taskRouter from "./src/routers/taskRouter.js";
