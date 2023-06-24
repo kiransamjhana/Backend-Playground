4a45e1787690caa43aa46017b3ec0fe25c807534
kiransamjhana <kiranmanandhar956@gmail.com>
2023-06-24 10:44:26 +1000

    env file

--- src/config/mongoDb.js
+++ src/config/mongoDb.js
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 export const mongoConnect = async () => {
   try {
+    const dbLink =
+      "mongodb+srv://<username>:0rHNgz7vp62hFczk@cluster0.ceqpjis.mongodb.net/?retryWrites=true&w=majority";
     const con = await mongoose.connect("mongodb://localhost:27017/nottododb");
 
     con && console.log("mongo is connected");
